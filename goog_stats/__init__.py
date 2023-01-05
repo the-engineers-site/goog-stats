b8a93e8015f4f41f0c88fac524202bba75001316
Yash Jagdale <jagdale0210@gmail.com>
2023-01-05 16:43:34 +0530

    simplified test cases

--- goog_stats/__init__.py
+++ goog_stats/__init__.py
@@ -1,41 +1,21 @@
 import os
+import time
 
 
 class Stats(object):
-    ERRORED, DISABLED_ENV, DISABLED, UNSET, ENABLED = range(5)
-
-    @property
-    def enabled(self):
-        return self.status in (Stats.UNSET, Stats.ENABLED)
-
-    @property
-    def enable_able(self):
-        return self.status not in (Stats.ERRORED, Stats.ENABLED)
-
-    @property
-    def disable_able(self):
-        return self.status not in (Stats.ERRORED, Stats.DISABLED)
-
-    @property
-    def recording(self):
-        return self.status in (Stats.UNSET, Stats.ENABLED)
-
-    @property
-    def sending(self):
-        return self.status is Stats.ENABLED
+    enabled: bool = False
+    start_time: time
 
     def __init__(self) -> None:
-        print(os.path)
+        print("starting stats collector")
+        self.start_time = time.time()
 
     def enable_stat(self):
         self.enabled = True
+        return
 
     def disable_stat(self):
         self.enabled = False
 
     def is_enabled(self):
         return self.enabled
-
-    @enabled.setter
-    def enabled(self, value: bool):
-        self.enabled = value
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,18 +4,13 @@ import pytest
 import goog_stats
 
 
-@pytest.mark.parametrize(
-    'text, expected'
-)
-def test_spacy(text, expected):
+def test_spacy():
     detector = goog_stats.Stats()
     detector.enable_stat()
     assert detector.is_enabled() is True
 
-@pytest.mark.parametrize(
-    'text, expected'
-)
-def test_spacy_disable(text, expected):
+
+def test_spacy_disable():
     detector = goog_stats.Stats()
     detector.disable_stat()
     assert detector.is_enabled() is False
