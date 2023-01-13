e449247fc77b5a791428db831633fe8238d04176
Yash Jagdale <jagdale0210@gmail.com>
2023-01-13 18:26:43 +0530

    test cases added

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -13,3 +13,14 @@ def test_spacy_disable():
     detector = goog_stats.Stats("test")
     detector.disable_stat()
     assert detector.is_enabled() is False
+
+
+def test_disable_status_post_disable():
+    detector = goog_stats.Stats("test")
+    assert detector.is_enabled() is False
+
+
+def test_enable_status_post_disable():
+    detector = goog_stats.Stats("test")
+    detector.enable_stat()
+    assert detector.is_enabled() is True
